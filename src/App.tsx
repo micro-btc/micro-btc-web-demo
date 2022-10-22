f9f4b773e07549167c46b13a98d93b4771593942
Ryan Shea <ryan@shea.io>
2022-10-22 14:24:31 -0400

    add taproot single public key

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,11 @@ const realPubKeys = [pubKey1, pubKey2, pubKey3];
   hex.decode('030000000000000000000000000000000000000000000000000000000000000001'),
   hex.decode('030000000000000000000000000000000000000000000000000000000000000002'),
   hex.decode('030000000000000000000000000000000000000000000000000000000000000003'),
-];*/
+];
+const dummySchnorrPubKeys = [
+  hex.decode('0101010101010101010101010101010101010101010101010101010101010101'),
+]
+*/
 
 // Witness public key hash
 const pubKeyInfo = btc.p2wpkh(pubKey1);
@@ -37,35 +41,103 @@ const multisigScriptHashInfo = btc.p2wsh(multisigScriptInfo);
 const multisigScriptHash = multisigScriptHashInfo.script;
 const multisigAddress = multisigScriptHashInfo.address;
 
+// Schnorr
+const schnorrPubKey1 = secp.schnorr.getPublicKey(privKey1);
+const schnorrPubKey2 = secp.schnorr.getPublicKey(privKey2);
+const schnorrPubKey3 = secp.schnorr.getPublicKey(privKey3);
+const schnorrPubKeys = [schnorrPubKey1, schnorrPubKey2, schnorrPubKey3];
+
+// Taproot of Simple public key
+const taprootScriptInfo = btc.p2tr(schnorrPubKey1);
+const taprootScript = taprootScriptInfo.script;
+const taprootAddress = taprootScriptInfo.address;
+
+// Taproot: Single Leaf 2/3 Multisig
+
+// Taproot: Multi Leaf 2/3 Multisig (EXPERIMENTAL)
+
 function App() {
   const [count, setCount] = useState(0)
+
+  const sections = [
+    {
+      "title": "Witness Public Key Hash",
+      "publicKeys": [toHex(pubKey1)],
+      "script": toHex(pubKeyScript),
+      "scriptHash": "",
+      "address": pkhAddress
+    },
+    {
+      "title": "Witness Script Hash: 2/3 Multisig",
+      "publicKeys": realPubKeys.map(p => toHex(p)),
+      "script": toHex(multisigScript),
+      "scriptHash": toHex(multisigScriptHash),
+      "address": multisigAddress
+    },
+    {
+      "title": "Taproot: Simple Public Key",
+      "publicKeys": [toHex(schnorrPubKey1)],
+      "script": toHex(taprootScript),
+      "address": taprootAddress
+    },
+    /*{
+      "title": "Taproot: 2/3 Multisig, Single-Leaf"
+    },
+    {
+      "title": "Taproot: 2/3 Multisig, Multi-Leaf"
+    }*/
+  ]
 
   return (
     <div className="App">
       <h1>Micro-BTC Web Demo</h1>
-      {/*<div className="card">
+ 
+      <div className='App-main'>
+        {sections.map((section, index) => (
+          <div key={index}>
+            <h2>{section.title}</h2>
+            {section.publicKeys ? <p>
+              <b>Public Keys:</b><br/>
+              {section.publicKeys.map((pubKey, index) => 
+                <span key={index}>{pubKey}<br/></span>
+              )}
+            </p> : null }
+            {section.script ? <p><b>Script:</b><br/>{section.script}</p> : null }
+            {section.scriptHash ?
+              <p><b>Script Hash:</b><br/>{section.scriptHash}</p>
+            : null }
+            {section.address ? <p><b>Address:</b><br/>{section.address}</p> : null }
+          </div>
+        ))}
+
+        {/*<h3>Witness Public Key Hash</h3>
+        <p><b>Public key:</b><br/>{toHex(pubKey1)}</p>
+        <p><b>Script:</b><br/>{toHex(pubKeyScript)}</p>
+        <p><b>Address:</b><br/>{pkhAddress}</p>
+
+        <h3>Witness Script Hash of a 2/3 Multisig</h3>
+        <p>
+          <b>Public Keys:</b><br/>
+          {realPubKeys.map(pubKey => 
+            <span>{toHex(pubKey)}<br/></span>
+          )}
+        </p>
+        <p><b>Script:</b><br/>{toHex(multisigScript)}</p>
+        <p><b>Script hash:</b><br/>{toHex(multisigScriptHash)}</p>
+          <p><b>Address:</b><br/>{multisigAddress}</p>*/}
+      </div>
+
+           {/*<div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>*/}
-      <div className='App-main'>
-        <h3>Witness Public Key Hash</h3>
-        <p><b>Public key:</b> {toHex(pubKey1)}</p>
-        <p><b>Script:</b> {toHex(pubKeyScript)}</p>
-        <p><b>Address:</b> {pkhAddress}</p>
 
-        <h3>Witness Script Hash of a Public Key Hash</h3>
+              {/*<h3>Witness Script Hash of a Public Key Hash</h3>
         <p><b>Public key:</b> {toHex(pubKey1)}</p>
         <p><b>Script:</b> {toHex(pubKeyHashScript)}</p>
         <p><b>Script hash:</b> {toHex(wshPubKeyHashScript)}</p>
-        <p><b>Address:</b> {wshPkhAddress}</p>
-
-        <h3>Witness Script Hash of a 2/3 Multisig</h3>
-        <p><b>Public Keys:</b> {toHex(pubKey1)}, {toHex(pubKey2)}, {toHex(pubKey3)} </p>
-        <p><b>Script:</b> {toHex(multisigScript)}</p>
-        <p><b>Script hash:</b> {toHex(multisigScriptHash)}</p>
-        <p><b>Address:</b> {multisigAddress}</p>
-      </div>
+        <p><b>Address:</b> {wshPkhAddress}</p>*/}
     </div>
   )
 }
